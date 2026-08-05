351ab5d0c0e480fa32f3d46438e317a25bf4a267
Pranay Kumar Masi <realpranay32@gmail.com>
2026-08-05 19:47:56 +0530

    M1: Implement PDF ingestion and document model

--- app/services/pdf_reader.py
+++ app/services/pdf_reader.py
@@ -5,16 +5,21 @@ def read_pdf(path:str):
 	# open a pdf using PYmupdf,Read every page,Extract txt from each page
 	#create a page obg for every page
 	#return document object
-	with pm.open(path) as doc:
-		doc = []
-		for page in doc:
-			each_p = Page(page.number,page.get_text)
-			doc.append(each_p)
-	Document(path,doc)
-	return doc
 	
-
-
+	with pm.open(path) as pdf:
+		pages = []
+		
+		for page in pdf:
+			each_p = Page(page.number,page.get_text())
+			pages.append(each_p)
+	document = Document(path,pages)
+	return document
+	
+document = read_pdf("data/sample/ACSE04.pdf")
+print(document)
+print(document.pages)
+print(len(document.pages))
+# print(document.pages[0].text[:300])
 	
 		
 		
